49a915d3f76247e790ca0196b983cdc6a6c39811
Michał Kurpiński <michal.kurpinski96@gmail.com>
2017-05-07 22:44:37 +0200

    Custom Pipes and Interfaces

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
-import { ProductListComponent }  from './products/product-list.component';
+import { ProductListComponent }  from './products/product-list.component'
+import { ProductFilterPipe }  from './products/product-filter.pipe'
 import { FormsModule }  from '@angular/forms';
 
 @NgModule({
   imports: [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, ProductListComponent ],
+  declarations: [ AppComponent, ProductListComponent,ProductFilterPipe ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
