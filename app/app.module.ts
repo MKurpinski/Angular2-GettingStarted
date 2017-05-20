2bade24767cf42382b41a37995ce90fc13dc4906
Michał Kurpiński <michal.kurpinski96@gmail.com>
2017-05-20 20:55:18 +0200

    Added service for products

--- app/app.module.ts
+++ app/app.module.ts
@@ -5,9 +5,10 @@ import { AppComponent }  from './app.component';
 import { ProductListComponent }  from './products/product-list.component'
 import { ProductFilterPipe }  from './products/product-filter.pipe'
 import { FormsModule }  from '@angular/forms';
+import { HttpModule }  from '@angular/http';
 
 @NgModule({
-  imports: [ BrowserModule, FormsModule ],
+  imports: [ BrowserModule, FormsModule, HttpModule ],
   declarations: [ AppComponent, ProductListComponent, ProductFilterPipe, StarComponent ],
   bootstrap: [ AppComponent ]
 })
